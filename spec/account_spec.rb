c4a11aa291d7ac0d2c4a3960733e4d583aab5d2b
zidehen <zoeidehen@gmail.com>
2022-01-31 14:38:23 +0000

    Passing test for withdrawing money from account & refactored tests

--- spec/account_spec.rb
+++ spec/account_spec.rb
@@ -1,16 +1,24 @@
 require 'account'
 
 describe Account do
+  subject(:account) {described_class.new}
+
   it "initializes with a balance of zero" do
-    account = Account.new
     expect(account.balance).to eq(0)
   end
 
   it { is_expected.to respond_to(:deposit).with(1).argument}
 
   it "can deposit money into the account" do
-    account = Account.new
     account.deposit(100)
     expect(account.balance).to eq(100)
+  end
+  
+  it { is_expected.to respond_to(:withdraw).with(1).argument}
+
+  it "can deposit money into the account" do
+    account.deposit(100)
+    account.withdraw(20)
+    expect(account.balance).to eq(80)
   end
 end
